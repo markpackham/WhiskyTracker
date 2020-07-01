d4a3e042be3132bfe6cf424e40b11f1ab6d5edb5
mark <markpackham1@gmail.com>
2020-07-01 15:50:27 +0100

    Initial commit

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
@@ -0,0 +1,31 @@
+package com.codeclan.example.WhiskyTracker.controllers;
+
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+//@RequestMapping(value = "/distilleries")
+public class DistilleryController {
+
+    @Autowired
+    DistilleryRepository distilleryRepository;
+
+    @GetMapping(value = "/distilleries")
+    //http://localhost:8080/distilleries?region=Highland
+    public ResponseEntity<List<Distillery>> getDistilleriesByRegion(@RequestParam(name = "region", required = false) String region){
+        if(region != null){
+            return new ResponseEntity<>(distilleryRepository.findDistilleryByRegion(region), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(distilleryRepository.findAll(), HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -0,0 +1,39 @@
+package com.codeclan.example.WhiskyTracker.controllers;
+
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+//@RequestMapping(value = "/whiskies")
+public class WhiskyController {
+
+    @Autowired
+    WhiskyRepository whiskyRepository;
+
+    @GetMapping(value = "/whiskies")
+    //http://localhost:8080/whiskies?year=2018
+    public ResponseEntity<List<Whisky>> findWhiskyByYear(@RequestParam(name="year", required = false) Integer year){
+        if(year != null){
+            return new ResponseEntity<>(whiskyRepository.findWhiskyByYear(year),HttpStatus.OK);
+        }
+        return new ResponseEntity<>(whiskyRepository.findAll(), HttpStatus.OK);
+    }
+
+    //Get all the whisky from a particular distillery that's a specific age
+
+    //http://localhost:8080/whiskies?distillery=GlendronachAndAge=2018
+    //http://localhost:8080/whiskies?distillery=Glendronach
+    @GetMapping(value = "/whiskies")
+    public ResponseEntity<List<Whisky>> findAllWhiskyByDistilleryAndYear(@RequestParam(name="distillery"))
+
+
+}
